53bea067a1de5b153a2d9e4e8afaf1f0969b9169
WhiteDevil2013 <william.wakim@epita.fr>
2016-12-15 21:48:01 +0100

    ajout de la fonctionnalité get url + bouton pour get l'adresse challengeme (défaillante)

--- app/src/main/java/benjamin/com/challengeme/MainActivity.java
+++ app/src/main/java/benjamin/com/challengeme/MainActivity.java
@@ -3,6 +3,7 @@ package benjamin.com.challengeme;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,18 +13,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener
 {
-
+    private Button myBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        myBtn = (Button) findViewById(R.id.button);
+        myBtn.setOnClickListener(this);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
@@ -114,5 +126,52 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public String get(String url) throws IOException {
+        InputStream is = null;
+        try {
+            final HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            is = conn.getInputStream();
+            // Read the InputStream and save it in a string
+            return readIt(is);
+        } finally {
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream is) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = r.readLine()) != null) {
+            response.append(line).append('\n');
+        }
+        return response.toString();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == myBtn)
+        {
+            try {
+                Log.d("myTag", "before get URL / button clicked");
+                get("http://challengeme.fr/get_challenges.php");
+                Log.d("myTag2", "end get URL with success");
+            } catch (IOException e) {
+                Log.e("myErrTag", "error catched while getting URL");
+                e.printStackTrace();
+            }
+        }
     }
 }
